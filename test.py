a081b938b6452b29ae4020baa18696b19e321daa
04harsh08 <12004harsh@gmail.com>
2021-10-21 17:11:58 +0100

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-args = ("scorecard", "--repo=github.com/ossf-tests/scorecard-check-branch-protection-e2e")
-popen = subprocess.Popen(args, stdout=subprocess.PIPE)
-popen.wait()
-output = popen.stdout.read()
-print(output)
